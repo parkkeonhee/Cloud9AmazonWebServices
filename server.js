bae03d7fc17fb128b7d92e81c438f3ab53fe1556
parkkeonhee <parkkeonhee@gmail.com>
2016-06-29 01:12:45 +0000

    cleaned up repositories and code

--- server.js
+++ server.js
@@ -16,42 +16,33 @@
  */
 
 // Required node_modules
-var async = require('async');
 var express = require('express');
 var formidable = require('formidable');
-var socketio = require('socket.io');
 
 // Setup server
 var http = require('http');
 var path = require('path');
+var redis = require('redis');
+var client = redis.createClient(6379, "myfirstcluster.lbf1q2.0001.use1.cache.amazonaws.com");
+
 var router = express();
 var server = http.createServer(router);
-var io = socketio.listen(server);
 
 router.use(express.static(path.resolve(__dirname, 'client')));
-var messages = [];
-var sockets = [];
 
 router.post('/', function(req, res) {
 
-  // Create an incoming form object
   var form = new formidable.IncomingForm();
 
   // Specify that we want to allow the user to upload multiple files in a single request
   form.multiples = true;
 
-  // Every time a file has been uploaded successfully, rename it to it's orignal name.
   form.on('file', function(field, file) {
-
     // Read a file from disk, store it in Redis, then read it back from Redis.
-    var redis = require("redis"),
-      client = redis.createClient(6379, "myfirstcluster.lbf1q2.0001.use1.cache.amazonaws.com", {
-        no_ready_check: true
-      }),
-      fs = require("fs"),
+      var fs = require("fs");
 
-      // Directory that stores the JSON file.
-      filename = file.path;
+    // Directory that stores the JSON file.
+      var filename = file.path;
 
     // Prints out the directory location of the uploaded JSON file.
     console.log("File successfully saved in: " + filename);
@@ -102,60 +93,6 @@ router.post('/', function(req, res) {
   // Parse the incoming request containing the form data.
   form.parse(req);
 });
-
-io.on('connection', function(socket) {
-  messages.forEach(function(data) {
-    socket.emit('message', data);
-  });
-
-  sockets.push(socket);
-
-  socket.on('disconnect', function() {
-    sockets.splice(sockets.indexOf(socket), 1);
-    updateRoster();
-  });
-
-  socket.on('message', function(msg) {
-    var text = String(msg || '');
-
-    if (!text)
-      return;
-
-    socket.get('name', function(err, name) {
-      var data = {
-        name: name,
-        text: text
-      };
-
-      broadcast('message', data);
-      messages.push(data);
-    });
-  });
-
-  socket.on('identify', function(name) {
-    socket.set('name', String(name || 'Anonymous'), function(err) {
-      updateRoster();
-    });
-  });
-});
-
-function updateRoster() {
-  async.map(
-    sockets,
-    function(socket, callback) {
-      socket.get('name', callback);
-    },
-    function(err, names) {
-      broadcast('roster', names);
-    }
-  );
-}
-
-function broadcast(event, data) {
-  sockets.forEach(function(socket) {
-    socket.emit(event, data);
-  });
-}
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
   var addr = server.address();
